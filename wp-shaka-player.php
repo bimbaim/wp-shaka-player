67ccf46e2408d04973c79ce665978276b79431cc
bimbaim <ibrahimquraisy@gmail.com>
2021-02-24 00:50:17 +0700

    Update wp-shaka-player.php

--- wp-shaka-player.php
+++ wp-shaka-player.php
@@ -43,7 +43,7 @@ add_action('wp_enqueue_scripts', 'my_load_scripts');
 
 
 function shaka_player_shortcode($atts){
-	ob_start();
+ob_start();
 	$args = shortcode_atts( 
 		array(
 			'm3u8'   => '',
@@ -64,10 +64,6 @@ function shaka_player_shortcode($atts){
 	
 	$link = $args['mpd'];
 	
-	if ( wonderplugin_is_device('iPhone,iPad') )
-		{
-			//$link = $args['m3u8'];
-		}
 	
 	if ( wonderplugin_is_device('Mobile') )
 	{
@@ -79,115 +75,69 @@ function shaka_player_shortcode($atts){
 		$link = $args['m3u8'];
 	}
 
-
-	if ( wonderplugin_is_browser('Chrome') )
-	{
-		//$link = $args['url_android'];
-	}
-
-
-
-	?>
-<style>
-#video-container {
-  display: inline-block;
-  position: relative;
-  text-align: center;
-  color: white;
-}
-#video-container p {
-    position: absolute;
-    /* top: 50%; */
-    /* left: 50%; */
-    bottom: 8px;
-    right: 16px;
-    transform: translate(-50%, -50%);
-    font-size: 50px;
-    opacity: 0.10;
-}
-
-</style>
+?>
 
 <script defer src="https://github.com/videojs/mux.js/releases/latest/download/mux.js"></script>
-<div id="video-container" align="center">
-	<p class="custom-message"><?php echo get_current_user_id(); ?></p>
-	<video id="video" width="100%" poster="<?php echo $args['poster']; ?>" <?php echo $autoplays; ?> ><p class="custom-message"><?php echo get_current_user_id(); ?></p></video>
-</div>
-<!-- <button onclick="var el=document.getElementById('video-container'); el.requestFullscreen();">Click here</button> -->
-	<script>
-
-	
-		
-		const manifestUri =
-			'<?php echo $link; ?>';
+<video data-shaka-player id="video" style="width:100%;height:100%" poster="<?php echo $args['poster']; ?>" <?php echo $autoplays; ?>></video>
+<script>
+const manifestUri =
+    '<?php echo $link; ?>';
 
 
-		function initApp() {
-		  // Install built-in polyfills to patch browser incompatibilities.
-		  shaka.polyfill.installAll();
-		
-		  // Check to see if the browser supports the basic APIs Shaka needs.
-		  if (shaka.Player.isBrowserSupported()) {
-			// Everything looks good!
-			initPlayer();
-		  } else {
-			// This browser does not have the minimum set of APIs we need.
-			console.error('Browser not supported!');
-		  }
-		}
-		
-		async function initPlayer() {
-		  // Create a Player instance.
-		  const video = document.getElementById('video');
-		  // Get a reference to the video container (a simple div which wraps the video)
-		  const videoContainer = document.getElementById('video-container');;
-		  // Construct a player
-		  const player = new shaka.Player(video);
-		  // Construct the UI overlay
-		  const ui = new shaka.ui.Overlay(player, videoContainer, video);
-		  const controls = ui.getControls();	
-		
-		  // Attach player to the window to make it easy to access in the JS console.
-		  window.player = player;
+async function init() {
+    // When using the UI, the player is made automatically by the UI object.
+    const video = document.getElementById('video');
+    const ui = video['ui'];
+    const controls = ui.getControls();
+    const player = controls.getPlayer();
 
-		 video.addEventListener('fullscreenchange', (event) => {
-			  if (document.fullscreenElement) {
-	
-				console.log(`Element: ${document.fullscreenElement.id} entered fullscreen mode.`);
+    // Attach player and ui to the window to make it easy to access in the JS console.
+    window.player = player;
+    window.ui = ui;
 
-			  } else {
-				console.log('Leaving full-screen mode.');
-			  }
-			});
+    // Listen for error events.
+    player.addEventListener('error', onPlayerErrorEvent);
+    controls.addEventListener('error', onUIErrorEvent);
 
-		  // Listen for error events.
-		  player.addEventListener('error', onErrorEvent);
-		
-		  // Try to load a manifest.
-		  // This is an asynchronous process.
-		  try {
-			await player.load(manifestUri);
-			// This runs if the asynchronous load is successful.
-			console.log('The video has now been loaded!');
-			  console.log(window);
-		  } catch (e) {
-			// onError is executed if the asynchronous load fails.
-			onError(e);
-		  }
-		}
-		
-		function onErrorEvent(event) {
-		  // Extract the shaka.util.Error object from the event.
-		  onError(event.detail);
-		}
-		
-		function onError(error) {
-		  // Log the error.
-		  console.error('Error code', error.code, 'object', error);
-		}
-		
-		document.addEventListener('DOMContentLoaded', initApp);
-	</script>
+    // Try to load a manifest.
+    // This is an asynchronous process.
+    try {
+        await player.load(manifestUri);
+        // This runs if the asynchronous load is successful.
+        console.log('The video has now been loaded!');
+    } catch (error) {
+        onPlayerError(error);
+    }
+}
+
+function onPlayerErrorEvent(errorEvent) {
+    // Extract the shaka.util.Error object from the event.
+    onPlayerError(event.detail);
+}
+
+function onPlayerError(error) {
+    // Handle player error
+    console.error('Error code', error.code, 'object', error);
+}
+
+function onUIErrorEvent(errorEvent) {
+    // Extract the shaka.util.Error object from the event.
+    onPlayerError(event.detail);
+}
+
+function initFailed(errorEvent) {
+    // Handle the failure to load; errorEvent.detail.reasonCode has a
+    // shaka.ui.FailReasonCode describing why.
+    console.error('Unable to load the UI library!');
+}
+
+// Listen to the custom shaka-ui-loaded event, to wait until the UI is loaded.
+document.addEventListener('shaka-ui-loaded', init);
+// Listen to the custom shaka-ui-load-failed event, in case Shaka Player fails
+// to load (e.g. due to lack of browser support).
+document.addEventListener('shaka-ui-load-failed', initFailed);
+</script>
+
 	<?php
 	return ob_get_clean();
 }
